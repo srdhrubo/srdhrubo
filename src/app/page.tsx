ec30bb1e0a2fde03649eef56ba31feae8be81cef
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-08-23 21:53:29 +0600

    added sharp package as recommended by nextjs for image optimization and fixed a link on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
               </Button>
             </div>
             <div>
-              <Link href="/contact">
+              <Link href="/contacts">
                 <button className="p-[3px] relative">
                   <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
                   <div className="flex gap-2 items-center px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white text-base hover:bg-transparent">

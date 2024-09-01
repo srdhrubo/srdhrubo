b03d37d1874ac00fc6d9ad6535c7c5cf845bd0b9
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-09-01 15:56:47 +0600

    changed tracing beam positioning a little bit

--- src/components/ui/tracing-beam.tsx
+++ src/components/ui/tracing-beam.tsx
@@ -51,7 +51,7 @@ export const TracingBeam = ({
       ref={ref}
       className={cn("relative w-full mx-auto h-full", className)}
     >
-      <div className="absolute -left-4 top-3">
+      <div className="absolute -left-[0.87rem] top-3 hidden lg:block">
         <motion.div
           transition={{
             duration: 0.2,

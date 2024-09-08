cfaa2bf82766e0e1667a5ae8d0d7ad02a7e25fb3
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-09-08 18:46:07 +0600

    added meteor-effect component and related animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,11 +82,20 @@ const config = {
           "0%, 100%": { opacity: "1" },
           "50%": { opacity: "0" },
         },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         blink: "blink 1s steps(1) infinite",
+        "meteor-effect": "meteor 5s linear infinite",
       },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],

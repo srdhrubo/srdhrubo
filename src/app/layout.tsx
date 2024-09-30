27347ed8cebd010bcd676e927d98cf468b1d1593
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-09-30 18:43:34 +0600

    added toaster to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavigationBar from "../components/NavBar";
 import Footer from "@/components/footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <NavigationBar />
         <main>{children}</main>
         <Footer />
+        <Toaster />
       </body>
     </html>
   );

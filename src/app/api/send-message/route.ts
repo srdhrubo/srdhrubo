ad5bd7322cb1f5543b6f7eff53674ab327b302cf
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-09-30 18:42:35 +0600

    added api for sending email

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, email, subject, message } = body;
+
+    if (!name || !email || !subject || !message) {
+      return NextResponse.json(
+        { error: "All fields are required" },
+        { status: 400 }
+      );
+    }
+
+    await transporter.sendMail({
+      from: `"Reach to Suchinto Roy Dhrubo" <${process.env.EMAIL_FROM}>`,
+      cc: email,
+      to: process.env.EMAIL_TO,
+      subject: `${name} is reaching out to you: ${subject}`,
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Subject: ${subject}
+        Message: ${message}
+      `,
+      html: `
+        <p>${message.replace(/\n/g, "<br>")}</p>
+      `,
+    });
+
+    return NextResponse.json(
+      { message: "Email sent successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return NextResponse.json(
+      { error: "Failed to send email" },
+      { status: 500 }
+    );
+  }
+}
